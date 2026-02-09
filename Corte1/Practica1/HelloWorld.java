58b3455e0e6cd258082f78ef11b0c11f5ceb0b59
josediazbohorquez-tech <jose.diaz.bohorquez@gmail.com>
2026-02-09 13:43:11 -0500

    Add files via upload

---
+++ Corte1/Practica1/HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+se utiliza para comentar
+*/
+package helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("¡Hola Mundo!");
+        
+    }
+}
